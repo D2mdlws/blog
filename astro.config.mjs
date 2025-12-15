c13d3809055c008e61efee3e670436a420128898
mdlws <mdlwswswws@outlook.com>
2025-12-15 16:42:47 +0800

    (post): init framework and post first article

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  },
+
+  integrations: [mdx()]
+});
